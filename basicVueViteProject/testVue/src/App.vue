b85f1b868a5322d9109625f1f411b5e094e2f081
Appu-Kumar <appu6688kumar@gmail.com>
2025-03-10 09:46:30 +0530

    feature:add how to pass props data as well as function

--- basicVueViteProject/testVue/src/App.vue
+++ basicVueViteProject/testVue/src/App.vue
@@ -1,40 +1,30 @@
-
-
 <script>
-  // This is option
-  export default {
-                    data(){
-                        return {
-                            actors:['ak','sk','shk','amitabh','kk-menon','bajpayee','rajneekant'],
-                            favActors:[],
-                        }
-                    },
-                    methods:{
-                        getFavActor(actor){
-                            this.favActors.push(actor);
-                        }
-                    }
-                }
+// This is option
+import ActorList from './components/ActorList.vue'
+
+export default {
+  components: { ActorList }, // registering the importing components
+  data() {
+    return {
+      actors: ['ak', 'sk', 'shk', 'amitabh', 'kk-menon', 'bajpayee', 'rajneekant'],
+      favActors: [],
+    }
+  },
+  methods: {
+    getFavActor(actor) {
+      this.favActors.push(actor)
+    },
+  },
+}
 </script>
 
-
 <template>
-        <ul>
-              <li v-for="(actor,idx) in actors" :key="idx">
-                 <p>{{actor}} </p>
-                <button v-on:click="getFavActor(actor)">Add Fav Actor</button>
-              </li>
-         </ul>
-
-         <p v-if="favActors.length === 0">There are no fav actors</p>
-         <div v-else>
-            Fav actors
-         <ul>
-               <li v-for="(favActor,idx) in favActors" :key="idx">{{favActor}}</li>
-         </ul>
-         </div>
+  <ActorList v-bind:actors="actors" v-bind:getFavActor="getFavActor" />
+  <p v-if="favActors.length === 0">There are no fav actors</p>
+  <div v-else>
+    Fav actors
+    <ul>
+      <li v-for="(favActor, idx) in favActors" :key="idx">{{ favActor }}</li>
+    </ul>
+  </div>
 </template>
-
-
-
-
---
+++ basicVueViteProject/testVue/src/components/ActorList.vue
@@ -0,0 +1,43 @@
+<script>
+export default {
+  //props: ['actors','getFavActor'],   // one of the way but let's write according to company standard
+  props:{
+     actors:{
+       type:Array,
+       required:true,
+     },
+     getFavActor:{
+       type:Function,
+       required:true,
+     }
+  },
+
+  data: () => ({
+  
+  }),
+  
+  // no use I am just writing to revise my concept
+  methods: {
+   
+  },
+  computed:{
+
+  },
+  watch:{
+
+  }
+
+}
+</script>
+
+
+<template>
+
+         <ul>
+              <li v-for="(actor,idx) in actors" :key="idx">
+                 <p>{{actor}} </p>
+                <button v-on:click="getFavActor(actor)">Add Fav Actor</button>
+              </li>
+         </ul>
+
+</template>
