ba74f49aaa6de8e7cb5195d2601535b421ef6ad7
Appu-Kumar <appu.kumar@greytip.com>
2025-03-16 12:17:53 +0530

    feature: add slot exercise which is in emitwala

--- basicVueViteProject/emitsWala/src/App.vue
+++ basicVueViteProject/emitsWala/src/App.vue
@@ -1,7 +1,8 @@
 <script>
    import ListOfColors from './components/ListOfColors.vue'
+   import ButtonBase from './components/button-base.vue'
    export default {
-     components:{ListOfColors},
+     components:{ListOfColors,ButtonBase},
      data(){
        return {
          favColorList:[],
@@ -17,12 +18,17 @@
 </script>
 
 <template>
-  
   <p> Hello emit  </p>
   <ListOfColors v-on:fav-color="addFavColor" />   <!-- Custom event -->
   <ul>
    <li v-for="(favColor,idx) in favColorList" v-bind:key="idx">{{favColor}} </li>
   </ul>
+  <!-- <Buttonbase text='small button' icon='icons'>LeftArrow text</Buttonbase>   <Inside's content of  this component as it is passed to the child's slot tag     -->
+  <ButtonBase> reset </ButtonBase>
+  <ButtonBase> cancel  </ButtonBase>
+  <ButtonBase> Reset   </ButtonBase>
+  <ButtonBase> clear  </ButtonBase>
+ 
 </template>
 
 
---
+++ basicVueViteProject/emitsWala/src/components/button-base.vue
@@ -0,0 +1,16 @@
+<!-- eslint-disable vue/require-prop-type-constructor -->
+<script>
+export default {
+  props: {
+    text: { type: String, required: true },
+    icon: { type: String, required: true },
+  },
+}
+</script>
+
+<template>
+  <!-- <button>{{ icon }} - {{ text }}</button> <br />   -->
+  <button> 
+      <slot> submit </slot>
+  </button>
+</template>
